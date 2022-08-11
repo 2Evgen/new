2fd44e50180584a4cf909bc84ca2c104b1ddf651
Evgeniya <evgeniya-e92@mail.ru>
2022-08-11 22:09:51 +0300

    100 % coverage

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -16,5 +16,18 @@ public class StatisticsServiceTest {
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void findMaxInTheMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 60, 8, 6, 11, 11, 12};
+        long expected = 60;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
     }
 }
